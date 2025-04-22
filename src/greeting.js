41522f8d4877a69aaf5a835784667b6e2b31aa0e
shaay98 <nrichardson567@icloud.com>
2025-04-22 02:07:33 +0000

    html import and export js files in src folder package json changes and a webpack.config file.

---
+++ src/greeting.js
@@ -0,0 +1,9 @@
+"use strict";
+
+export function sayHello(Charlie) {
+  return `Hello, ${Charlie}!`;
+}
+
+export function sayGoodbye(Charlie) {
+  return `Goodbye, ${Charlie}!`;
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+import { sayHello, sayGoodbye } from "./greeting.js";
+
+console.log(sayHello("Webpack"));
+console.log(sayGoodbye("Webpack"));
